6579b43bfca38abf24bb1d33bb44b645c922d7e2
dhaval7717 <135348304+dhaval7717@users.noreply.github.com>
2023-09-17 18:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Your Script here.
-
 const lookup = {
   A: "N",
   B: "O",
@@ -32,15 +30,17 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return; //return decodedArr
+  let decodedArr = [];
+  for (let i = 0; i < encodedStr.length; i++) {
+    const char = encodedStr[i];
+    if (lookup.hasOwnProperty(char)) {
+      decodedArr.push(lookup[char]);
+    } else {
+      decodedArr.push(char);
+    }
+  }
+  return decodedArr.join('');
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
+// You can test the function with your example:
+console.log(rot13("SERR YBIR? NPPVBWBO"));
